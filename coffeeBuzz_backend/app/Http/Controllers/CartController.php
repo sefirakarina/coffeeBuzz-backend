76d4cab51bfba5110d23e8c636b4499462b7a4cd
veronika98stephanie <veronikastephanie789@gmail.com>
2019-04-21 22:04:39 +1000

    fixed(Cart): fixed cart controller and api

--- coffeeBuzz_backend/app/Http/Controllers/CartController.php
+++ coffeeBuzz_backend/app/Http/Controllers/CartController.php
@@ -8,27 +8,77 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    public function showAllCarts() {
-        return Cart::all();
+    protected $cart;
+
+    public function __construct(Cart $cart)
+    {
+        $this->middleware('auth:api');
+        $this->cart = $cart;
     }
 
-    public function showCartById($id) {
-        return Cart::find($id);
+    public function index()
+    {
+        $cart = Cart::all();
+        $array = Array();
+        $array['data'] = $cart;
+        if (count($cart) > 0) {
+            return response()->json($array, 200);
+        } else {
+            return response()->json(['error' => 'Cart not found'], 404);
+        }
     }
 
-    public function deleteCart($id) {
-        Cart::where('id', $id)->delete();
+    public function show($id)
+    {
+        $cart = Cart::find($id);
+        $array = Array();
+        $array['data'] = $cart;
+        if (count($cart) > 0) {
+            return response()->json($array, 200);
+        } else {
+            return response()->json(['error' => 'Cart not found'], 404);
+        }
     }
 
-    public function addCart(Request $request) {
-        $cart = new Cart;
-        $cart->user_id = $request->user_id;
-        $cart->save();
+    public function destroy($id, Request $request)
+    {
+        $cart = Cart::where('id', $id)->delete();
+        if ($cart != null) {
+            return response()->json($cart, 200);
+        } else {
+            return response()->json(['error' => 'Cart cannot be deleted'], 404);
+        }
     }
 
-    public function updateCart(Request $request, $id) {
+    public function store(Request $request)
+    {
+        $newCart = [
+            'user_id' => $request->user_id,
+        ];
+
+        if ($newCart != null) {
+            $new = $this->cart->create($newCart);
+            $array = Array();
+            $array['data'] = $new;
+            return response()->json($array, 200);
+        } else {
+            return response()->json(['error' => 'Cart not added'], 404);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
         $cart = Cart::find($id);
         $cart->user_id = $request->input('user_id');
         $cart->save();
+
+        $cart = Cart::where('id', $request->id)->update([
+            'user_id' => $request->user_id,
+        ]);
+        if($cart!=null){
+            return response()->json($cart, 200);
+        } else {
+            return response()->json(['error' => 'Cart not updated'], 404);
+        }
     }
 }
