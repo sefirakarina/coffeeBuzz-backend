7635f58a465d90f2e50e133cad7463e634fcb0f9
veronika98stephanie <veronikastephanie789@gmail.com>
2019-05-01 23:42:09 +1000

    adding routes exception

--- coffeeBuzz_backend/app/Http/Controllers/UsersController.php
+++ coffeeBuzz_backend/app/Http/Controllers/UsersController.php
@@ -11,7 +11,7 @@ class UsersController extends Controller
 
     public function __construct(User $user)
     {
-        $this->middleware('auth:api');
+        $this->middleware('auth:api', ['except' => ['store']]);
         $this->user = $user;
     }
 
